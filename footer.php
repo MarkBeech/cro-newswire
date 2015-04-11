e2c41d7a6f01137fbd492be320504b87b5944351
Mark Beech <maqist@maqist.net>
2015-04-11 15:29:01 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+	</div><!-- #container -->
+	<div class="push"></div>
+</div><!-- #wrapper -->
+<footer id="colophon" role="contentinfo">
+		<div id="site-generator">
+&copy; Crocels&nbsp;News 
+		</div>
+	</footer><!-- #colophon -->
+<?php wp_footer(); ?>
+</body>
+</html>
