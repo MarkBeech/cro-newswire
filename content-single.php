d3d884727317702ae66234ef338bbe57f067c4af
Mark Beech <maqist@maqist.net>
2015-04-11 15:24:12 +0100

    Update content-single.php
    Imprint and legals mangled

--- content-single.php
+++ content-single.php
@@ -14,7 +14,43 @@
 		<div class="colorbar"></div>
    
     <footer class="entry-meta">
-    <?php
+    <?php edit_post_link( __( 'Edit', 'newswire' ), '<span class="edit-link">', '</span>' ); ?>
+        <div class="entry-meta">
+			
+			<?php 
+if( get_post_meta($post->ID, 'jb_imprint', true) ) {
+
+echo get_post_meta($post->ID, 'jb_imprint', true);
+echo ('&nbsp;');
+}
+?><?php newswire_posted_on(); ?> <!-- byline moved to bottom of article -->			<?php
+$postlicensecatid=0;
+
+if(in_category(466)) {
+$postlicensecatid=466;
+} elseif(in_category(467)) {
+$postlicensecatid=467;
+} elseif(in_category(468)) {
+$postlicensecatid=468;
+} elseif(in_category(469)) {
+$postlicensecatid=469;
+} elseif(in_category(470)) {
+$postlicensecatid=470;
+} elseif(in_category(471)) {
+$postlicensecatid=471;
+} else {
+$postlicensecatid=469;
+}
+
+
+if ($postlicensecatid!=0) {
+	
+?>
+<?php $licence= category_description($postlicensecatid ); ?>
+<?php echo 'and is ', $licence ?><?php
+}
+?>
+    <br/><?php
         /* translators: used between list items, there is a space after the comma */
         $tag_list = get_the_tag_list( '', ', ' );
 
@@ -23,9 +59,6 @@
             printf ( __( '<span class="tag-meta">Tagged %s</span>', 'newswire' ), $tag_list);
         }
     ?>
-    <?php edit_post_link( __( 'Edit', 'newswire' ), '<span class="edit-link">', '</span>' ); ?>
-        <div class="entry-meta">
-			<?php newswire_posted_on(); ?> <!-- byline moved to bottom of article -->
 		</div><!-- .entry-meta -->
     </footer><!-- .entry-meta -->
          
