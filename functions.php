5563dd58632fae7bbbefb84f52b089a415b2e07d
Mark Beech <mark@beech.org.uk>
2015-07-17 14:33:45 +0100

    Update functions.php
    Adding Advertisement (Widget) function

--- functions.php
+++ functions.php
@@ -7,4 +7,17 @@ function theme_enqueue_styles() {
     );
 }
 remove_filter('term_description','wpautop');
+register_sidebars( 1,
+array(
+'name' => 'AddBar',
+'before_widget' => '
+<div id="%1$s" class="widget %2$s">',
+'after_widget' => '</div>
+',
+'before_title' => '
+<h2 class="widgettitle">',
+'after_title' => '</h2>
+'
+)
+);
 ?>
